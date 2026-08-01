567090bdc060685b8806a9c4c6ba7c61549aec44
lalike04-dev <lalike04@gmail.com>
2026-08-01 23:50:00 +0300

    adding some members for futher operation later on

---
+++ data/members.js
@@ -0,0 +1,8 @@
+export let memberRecord = [
+    {id:1 , firstname: "Anteneh", lastname: "Mulatu", email: "an225mul@gmail.com", phone: null},
+    {id:2 , firstname: "Abel", lastname: "Muluneh", email: "ab2350@gmail.com", phone: 978243544},
+    {id:3 , firstname: "Bethelhem", lastname: "Raey", email: "RaeyBeth@gmail.com", phone: 980006574},
+    {id:4 , firstname: "Gobeze", lastname: "Birtu", email: "GB234@gmail.com", phone: 700861211},
+    {id:5 , firstname: "Melat", lastname: "Abyot", email: "abi6melu@gmail.com", phone: null},
+    {id:6 , firstname: "Netsanet", lastname: "Fikir", email: "netsi737@gmail.com", phone: null},
+]
