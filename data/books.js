5cf27406942f4e24b3d3fcbcbc9bac4224f15bde
lalike04-dev <lalike04@gmail.com>
2026-08-02 00:05:19 +0300

    adding some borrow services that has been done in the past

--- data/books.js
+++ data/books.js
@@ -1,8 +1,8 @@
 export let booksRecord = [
-    {id: 1, title: "venom", author: null, category: "Fiction", publicationyear: 2015, isbn: 987403722, totalcopies: 15, availablecopies: 13},//2
+    {id: 1, title: "venom", author: null, category: "Fiction", publicationyear: 2015, isbn: 987403722, totalcopies: 15, availablecopies: 14},//1
     {id: 2, title: "How to Sleep", author: "Fredrich Nische", category: "Science", publicationyear: 1879, isbn: 100912742, totalcopies: 3, availablecopies: 3},
-    {id: 3, title: "A play by the pond", author: "Nancy Ferdinand", category: "Fiction", publicationyear: 2012, isbn: 9560722, totalcopies: 5, availablecopies: 3},//2
+    {id: 3, title: "A play by the pond", author: "Nancy Ferdinand", category: "Fiction", publicationyear: 2012, isbn: 9560722, totalcopies: 5, availablecopies: 5},
     {id: 4, title: "Dune", author: "Frank Herbert", category: "Sci-fi", publicationyear: 1965, isbn: 2736409463, totalcopies: 5, availablecopies: 3},//2
     {id: 5, title: "Dune Messiah", author: "Frank Herbert", category: "Sci-fi", publicationyear: 1969, isbn: 782464732, totalcopies: 5, availablecopies: 4},//1
-    {id: 6, title: "Children of Dune", author: "Frank Herbert", category: "Sci-fi", publicationyear: 1976, isbn: 7823424732, totalcopies: 5, availablecopies: 4}//1
+    {id: 6, title: "Children of Dune", author: "Frank Herbert", category: "Sci-fi", publicationyear: 1976, isbn: 7823424732, totalcopies: 5, availablecopies: 5}
 ]
---
+++ data/borrowed-books.js
@@ -0,0 +1,6 @@
+export let borrowRecord = [
+    {memberid: 2, bookid: 1, borrowdate: "2025-9-1", hasreturned: true},
+    {memberid: 5, bookid: 4, borrowdate: "2026-4-15", hasreturned: true},
+    {memberid: 6, bookid: 5, borrowdate: "2026-4-15", hasreturned: false},
+    {memberid: 4, bookid: 4, borrowdate: "2026-1-23", hasreturned: false}
+]
