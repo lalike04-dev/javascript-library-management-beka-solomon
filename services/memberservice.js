a03e51a578743ef410c78ddb332fc0de8705df56
lalike04-dev <lalike04@gmail.com>
2026-08-03 00:13:48 +0300

    commenting unnecessary code lines

--- services/memberservice.js
+++ services/memberservice.js
@@ -1,18 +1,12 @@
-import { memberRecord } from "../data/members.js";
-import { borrowRecord } from "../data/borrowed-books.js";
-export function addMember(
-  id,
-  firstName,
-  lastName,
-  email,
-  phone
-) {
+/* import { memberRecord } from "../data/members.js";
+import { borrowRecord } from "../data/borrowed-books.js"; */
+export function addMember(id, firstName, lastName, email, phone) {
   let insert = {
     id: id,
     firstName: firstName,
     lastName: lastName,
     email: email,
-    phone: phone
+    phone: phone,
   };
   memberRecord.push(insert);
 }
@@ -23,14 +17,14 @@ console.log(memberRecord); */
 export function viewAllMembers() {
   for (let i = 0; i < memberRecord.length; i++) {
     let {
-    id: id,
-    firstname: firstName,
-    lastname: lastName,
-    email: email,
-    phone: phone
+      id: id,
+      firstname: firstName,
+      lastname: lastName,
+      email: email,
+      phone: phone,
     } = memberRecord[i];
     console.log(
-      `Member${i}->id:${id}, First name:${firstName}, last name:${lastName}, email:${email}, phone:${phone}`
+      `Member${i}->id:${id}, First name:${firstName}, last name:${lastName}, email:${email}, phone:${phone}`,
     );
   }
 }
@@ -48,10 +42,10 @@ export function searchMember(choice, id, firstname) {
             firstname: firstName,
             lastname: lastName,
             email: email,
-            phone: phone
-            } = memberRecord[i];
+            phone: phone,
+          } = memberRecord[i];
           console.log(
-            `id:${id}, fisrtName:${firstName}, lastName:${lastName}, email:${email}, phone:${phone}`
+            `id:${id}, fisrtName:${firstName}, lastName:${lastName}, email:${email}, phone:${phone}`,
           );
           break;
         }
@@ -63,17 +57,19 @@ export function searchMember(choice, id, firstname) {
     case "firstname":
       let truth2 = true;
       for (let i = 0; i < memberRecord.length; i++) {
-            if (firstname.toLowerCase() == memberRecord[i].firstname.toLowerCase()) {
+        if (
+          firstname.toLowerCase() == memberRecord[i].firstname.toLowerCase()
+        ) {
           truth2 = true;
           let {
             id: id,
             firstname: firstName,
             lastname: lastName,
             email: email,
-            phone: phone
-            } = memberRecord[i];
+            phone: phone,
+          } = memberRecord[i];
           console.log(
-            `id:${id}, fisrtName:${firstName}, lastName:${lastName}, email:${email}, phone:${phone}`
+            `id:${id}, fisrtName:${firstName}, lastName:${lastName}, email:${email}, phone:${phone}`,
           );
           break;
         }
@@ -89,38 +85,51 @@ export function searchMember(choice, id, firstname) {
 //searchMember("id", 9, "Melu");
 
 export function updateMember(id, firstname, lastname, email, phone) {
-  
-      let truth = true;
-      for (let i = 0; i < memberRecord.length; i++) {
-        if (id == memberRecord[i].id) {
-          truth = true;
-            memberRecord[i].firstname=firstname;
-            memberRecord[i].lastname=lastname;
-            memberRecord[i].email=email;
-            memberRecord[i].phone=phone;
-            console.log("Updated Succesfully!");
-          break;
-        }
-        truth = false;
-      }
-      if (!truth) console.log("Member doesnt exist");
-    }
-//updateMember(9,"feta","belu","feta@gmail.com",null);
-
-export function deleteMember(id) {
   let truth = true;
   for (let i = 0; i < memberRecord.length; i++) {
     if (id == memberRecord[i].id) {
-    for(let j=0; j<borrowRecord.length; j++){
-      if(borrowRecord[j].hasreturned){
-      memberRecord.splice(i, 1);
       truth = true;
-      console.log("Deleted successfully");
-      break;}
+      memberRecord[i].firstname = firstname;
+      memberRecord[i].lastname = lastname;
+      memberRecord[i].email = email;
+      memberRecord[i].phone = phone;
+      console.log("Updated Succesfully!");
+      break;
+    }
     truth = false;
-    }
-    }
-    }
-  if (!truth) console.log("Member doesnt exist!");
+  }
+  if (!truth) console.log("Member doesnt exist");
 }
-//deleteMember(6);
+//updateMember(9,"feta","belu","feta@gmail.com",null);
+
+export function deleteMember(id) {
+let i=0;
+let j=0;
+let truth=true;
+let truth2=true;
+for(;i<memberRecord.length; i++){
+    if(id==memberRecord[i].id){
+        truth=true;
+        break;
+    }
+    truth=false;
+}
+
+for(; j<borrowRecord.length; j++){
+    if(id==borrowRecord[j].memberid){
+        truth=true;
+        break;
+    }
+    truth=false;
+}
+if(truth && truth2){
+    if(borrowRecord[j].hasreturned){
+        memberRecord.splice(i, 1);
+        console.log("Deleted Sucessfully");
+    }
+    else console.log("Member hasnt returned the borrowed book!");
+
+}
+else console.log("Member doesnt exist or member have not borrowed a book yet");
+}
+//deleteMember(2);
