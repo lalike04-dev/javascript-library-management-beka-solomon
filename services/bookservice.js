4ad12a25fc916a70a5f7fe11abf452267f29962f
lalike04-dev <lalike04@gmail.com>
2026-08-02 16:58:51 +0300

    working on the searching feature by id. i will add title, auther and category search shortly after

--- services/bookservice.js
+++ services/bookservice.js
@@ -1,6 +1,7 @@
-//import { booksRecord } from "../data/books.js";
+/* import { booksRecord } from "../data/books.js"; */
 
-//import { booksRecord } from "../data/books.js";
+import { booksRecord } from "../data/books";
+
 export function addBook(
   id,
   title,
@@ -26,17 +27,44 @@ export function addBook(
 
 /* addBook(1, "tit", "henesey", "fiction", 2015, 123445645, 2, 0);
 console.log(booksRecord); */
-export function viewallbooks(){
-    let id;
-    let title;
-    let author;
-    let category;
-    let publicationyear;
-    let isbn;
-    let totalcopies;
-    let availablecopies;
-    for(let i=0; i< booksRecord.length; i++){
-        {id,title,author,category,publicationyear,isbn,totalcopies,availablecopies,}=booksRecord[i];
-        console.log(`id:${id}, Title:${title}, author:${author}, category:${category}, publicationyear:${publicationyear}, isbn:${isbn}, totalcopies:${totalcopies}, availablecopies:${availablecopies}`);
+export function viewAllBooks() {
+  for (let i = 0; i < booksRecord.length; i++) {
+    let {
+      id,
+      title,
+      author,
+      category,
+      publicationyear,
+      isbn,
+      totalcopies,
+      availablecopies,
+    } = booksRecord[i];
+    console.log(
+      `Book${i}->id:${id}, Title:${title}, author:${author}, category:${category}, publicationyear:${publicationyear}, isbn:${isbn}, totalcopies:${totalcopies}, availablecopies:${availablecopies}`,
+    );
+  }
+}
+/* viewAllBooks(); */
+
+export function searchBook(choice, id, title, author, category){
+    switch(choice.toLowerCase()){
+        case "id":
+            for (let i=0; i<booksRecord.length; i++){
+                if(id==booksRecord[i].id){
+                let {
+                id,
+                title,
+                author,
+                category,
+                publicationyear,
+                isbn,
+                totalcopies,
+                availablecopies,
+                } = booksRecord[i];
+                console.log(`id:${id}, Title:${title}, author:${author}, category:${category}, publicationyear:${publicationyear}, isbn:${isbn}, totalcopies:${totalcopies}, availablecopies:${availablecopies}`,);
+            }
+        }
+        break;
+    
     }
 }
