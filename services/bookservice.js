adf33e80dc397b1d3fcef244536eb7f643b97523
lalike04-dev <lalike04@gmail.com>
2026-08-02 17:40:27 +0300

    adding defualt option for the program to fall back on and testing the functionality of the search feature in general

--- services/bookservice.js
+++ services/bookservice.js
@@ -1,6 +1,4 @@
-/* import { booksRecord } from "../data/books.js"; */
-
-import { booksRecord } from "../data/books";
+import { booksRecord } from "../data/books.js";
 
 export function addBook(
   id,
@@ -46,79 +44,85 @@ export function viewAllBooks() {
 }
 /* viewAllBooks(); */
 
-export function searchBook(choice, id, title, author, category){
-    switch(choice.toLowerCase()){
-        case "id":
-            for (let i=0; i<booksRecord.length; i++){
-                if(id==booksRecord[i].id){
-                let {
-                id,
-                title,
-                author,
-                category,
-                publicationyear,
-                isbn,
-                totalcopies,
-                availablecopies,
-                } = booksRecord[i];
-                console.log(`id:${id}, Title:${title}, author:${author}, category:${category}, publicationyear:${publicationyear}, isbn:${isbn}, totalcopies:${totalcopies}, availablecopies:${availablecopies}`,);
-            }
-        }
-        break;
 
-        case "title":
-            for (let i=0; i<booksRecord.length; i++){
-                if(title==booksRecord[i].title){
-                let {
-                id,
-                title,
-                author,
-                category,
-                publicationyear,
-                isbn,
-                totalcopies,
-                availablecopies,
-                } = booksRecord[i];
-                console.log(`id:${id}, Title:${title}, author:${author}, category:${category}, publicationyear:${publicationyear}, isbn:${isbn}, totalcopies:${totalcopies}, availablecopies:${availablecopies}`,);
-            }
+export function searchBook(choice, id, title, author, category) {
+  switch (choice.toLowerCase()) {
+    case "id":
+      for (let i = 0; i < booksRecord.length; i++) {
+        if (id == booksRecord[i].id) {
+          let {
+            id,
+            title,
+            author,
+            category,
+            publicationyear,
+            isbn,
+            totalcopies,
+            availablecopies,
+          } = booksRecord[i];
+          console.log(`id:${id}, Title:${title}, author:${author}, category:${category}, publicationyear:${publicationyear}, isbn:${isbn}, totalcopies:${totalcopies}, availablecopies:${availablecopies}`);
         }
-        break;
+      }
+      break;
 
-        case "author":
-            for (let i=0; i<booksRecord.length; i++){
-                if(author==booksRecord[i].author){
-                let {
-                id,
-                title,
-                author,
-                category,
-                publicationyear,
-                isbn,
-                totalcopies,
-                availablecopies,
-                } = booksRecord[i];
-                console.log(`id:${id}, Title:${title}, author:${author}, category:${category}, publicationyear:${publicationyear}, isbn:${isbn}, totalcopies:${totalcopies}, availablecopies:${availablecopies}`,);
-            }
+    case "title":
+      for (let i = 0; i < booksRecord.length; i++) {
+            
+        if (title.toLowerCase() == booksRecord[i].title.toLowerCase()) {
+            let {
+            id,
+            title,
+            author,
+            category,
+            publicationyear,
+            isbn,
+            totalcopies,
+            availablecopies,
+          } = booksRecord[i];
+          console.log(`id:${id}, Title:${title}, author:${author}, category:${category}, publicationyear:${publicationyear}, isbn:${isbn}, totalcopies:${totalcopies}, availablecopies:${availablecopies}`);
         }
-        break;
+      }
+      break;
 
-        case "category":
-            for (let i=0; i<booksRecord.length; i++){
-                if(category==booksRecord[i].category){
-                let {
-                id,
-                title,
-                author,
-                category,
-                publicationyear,
-                isbn,
-                totalcopies,
-                availablecopies,
-                } = booksRecord[i];
-                console.log(`id:${id}, Title:${title}, author:${author}, category:${category}, publicationyear:${publicationyear}, isbn:${isbn}, totalcopies:${totalcopies}, availablecopies:${availablecopies}`,);
-            }
+    case "author":
+      for (let i = 0; i < booksRecord.length; i++) {
+        if (author == booksRecord[i].author) {
+          let {
+            id,
+            title,
+            author,
+            category,
+            publicationyear,
+            isbn,
+            totalcopies,
+            availablecopies,
+          } = booksRecord[i];
+          console.log(`id:${id}, Title:${title}, author:${author}, category:${category}, publicationyear:${publicationyear}, isbn:${isbn}, totalcopies:${totalcopies}, availablecopies:${availablecopies}`);
         }
-        break;
-    
-    }
+      }
+      break;
+
+    case "category":
+      for (let i = 0; i < booksRecord.length; i++) {
+        if (category.toLowerCase() == booksRecord[i].category.toLowerCase()) {
+          let {
+            id,
+            title,
+            author,
+            category,
+            publicationyear,
+            isbn,
+            totalcopies,
+            availablecopies,
+          } = booksRecord[i];
+          console.log(`id:${id}, Title:${title}, author:${author}, category:${category}, publicationyear:${publicationyear}, isbn:${isbn}, totalcopies:${totalcopies}, availablecopies:${availablecopies}`);
+        }
+      }
+      break;
+
+    default:
+      console.log("Error, incompatable input...try again!");
+  }
 }
+
+//searchBook("category", 6, "Dune Messiah", "Fredrich Nische", "Sci-fi");
